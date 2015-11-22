87e5b993a0d51b92a524495764eed7bc467bec51
KolrowyAleksander <ollo17sz@gmail.com>
2015-11-23 00:23:19 +0100

    main
    Input tekstu

--- generatorWierszy/main.cpp
+++ generatorWierszy/main.cpp
@@ -19,11 +19,16 @@ int main(){
 	Dictionary dict;
 
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Generator",sf::Style::None); //create a sfml renderwindow
+
+	sf::String Text;
+	string input_text;
+	//the following part of code is based on sfml tutorial foun on sfml site, its common way to handle the window
+	
 	while (window.isOpen()){
 		sf::Event event;
 		sf::Font font;	//instance of font class
 		if (!font.loadFromFile("Paper Banner-Regular.ttf")){} //loading font from file
-		sf::Text text("Hello world", font, 36);	//instance of text class
+		sf::Text text(input_text, font, 36);	//instance of text class
 		text.setColor(sf::Color::Red); //color
 		while (window.pollEvent(event)){
 			// ReSharper disable once CppIncompleteSwitchStatement
@@ -35,6 +40,15 @@ int main(){
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 					window.close();
 				break;
+			case sf::Event::TextEntered:
+				if (event.text.unicode == '\b' && input_text.size()){
+					input_text.pop_back();
+					std::cout << input_text << std::endl;
+				} else if (event.text.unicode < 128){
+					input_text += static_cast<char>(event.text.unicode);
+					std::cout << input_text << std::endl;
+				}
+				
 			}
 		}
 
