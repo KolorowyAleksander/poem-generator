832e3954c501a09cd2bec5351ab9b393c6ca2013
KolrowyAleksander <ollo17sz@gmail.com>
2015-11-23 03:16:42 +0100

    Ikonki
    Ikonki i nowa czcionka i wzor ui

--- generatorWierszy/main.cpp
+++ generatorWierszy/main.cpp
@@ -17,23 +17,32 @@ int main(){
 	//initialization of needed things
 	SentencePattern pattern; //patterns
 	Dictionary dict; //dictionary
-	sf::RenderWindow window(sf::VideoMode(500, 550), "Generator",sf::Style::None); //create a sfml renderwindow
-
+	sf::RenderWindow window(sf::VideoMode(570, 750), "Generator",sf::Style::None); //create a sfml renderwindow
 
 	sf::String Text;
 	sf::String Title;
-	sf::RectangleShape rectangle(sf::Vector2f(250, 75));
-	rectangle.setFillColor(sf::Color(100, 100, 100));
-	rectangle.move(sf::Vector2f(10, 10));
+	sf::RectangleShape rectangle(sf::Vector2f(250, 50));
+	sf::RectangleShape rectangle2(sf::Vector2f(250, 50));
+	sf::RectangleShape rectangle3(sf::Vector2f(250, 50));
+	sf::RectangleShape rectangle4(sf::Vector2f(250, 50));
+	rectangle.move(sf::Vector2f(20, 20));
+	rectangle2.move(sf::Vector2f(290, 20));
+	rectangle3.move(sf::Vector2f(20, 90));
+	rectangle4.move(sf::Vector2f(290, 90));
+	sf::Texture texture;
+	if (!texture.loadFromFile("edit.png")){}
+	sf::Sprite sprite(texture);
+	sprite.move(20, 690);
 	string input_text;
+
 	//the following part of code is based on sfml tutorial foun on sfml site, its common way to handle the window
 	
 	while (window.isOpen()){
 		sf::Event event;
 		sf::Font font;	//instance of font class
-		if (!font.loadFromFile("Pollito Peligroso.ttf")){} //loading font from file
-		sf::Text text(input_text, font, 50);	//instance of text class
-		text.move(sf::Vector2f(5, 5));
+		if (!font.loadFromFile("NotoSans-Bold.ttf")){} //loading font from file
+		sf::Text text(input_text, font, 20);	//instance of text class
+		text.move(sf::Vector2f(20, 140));
 		text.setColor(sf::Color::White); //color
 		while (window.pollEvent(event)){
 			// ReSharper disable once CppIncompleteSwitchStatement
@@ -53,13 +62,20 @@ int main(){
 					input_text += static_cast<char>(event.text.unicode);
 					std::cout << input_text << std::endl;
 				}
-
+				break;
+			//case sf::Mouse::Button::Left:
+				//where clicked
+				//;
 			}
 		}
 
 		window.clear();
 		window.draw(rectangle);
+		window.draw(rectangle2);
+		window.draw(rectangle3);
+		window.draw(rectangle4);
 		window.draw(text);
+		window.draw(sprite);
 		window.display();
 	}
 	//here go every de-initialization methods we need / also saving them to file and stuff
